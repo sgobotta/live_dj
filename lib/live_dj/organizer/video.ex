3b0bbeda15898de40726fa55b7012e591adba60c
Wasabi Brownies <makesmereal@gmail.com>
2020-10-18 15:36:06 -0300

    :art: Adds styles for the search list buttons

--- lib/live_dj/organizer/video.ex
+++ lib/live_dj/organizer/video.ex
@@ -2,7 +2,7 @@ defmodule LiveDj.Organizer.Video do
 
   alias LiveDj.Organizer.Video
 
-  defstruct channel_title: "", description: "", img_height: "", img_url: "", img_width: "", is_queued: "", title: "", video_id: "", previous: "", next: ""
+  defstruct channel_title: "", description: "", img_height: "", img_url: "", img_width: "", is_queued: false, title: "", video_id: "", previous: "", next: ""
 
   def update(video, props) do
     Map.merge(video, props)
@@ -14,7 +14,7 @@ defmodule LiveDj.Organizer.Video do
       img_url: tubex_video.thumbnails["default"]["url"],
       img_height: tubex_video.thumbnails["default"]["height"],
       img_width: tubex_video.thumbnails["default"]["width"],
-      is_queued: "",
+      is_queued: false,
       description: tubex_video.description,
       title: tubex_video.title,
       video_id: tubex_video.video_id,
