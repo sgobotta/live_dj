70427ed1f1929a45ca39da3c7237176cf5a63331
Santiago <santiago@camba.coop>
2020-11-01 04:23:48 -0300

    :computer: Adds the ChatSyncing hook to the socket

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ import {LiveSocket} from "phoenix_live_view"
 import PlayerSyncing from "./hooks/player-syncing";
 import PresenceSyncing from "./hooks/presence-syncing";
 import DragAndDropping from "./hooks/drag-and-dropping";
+import ChatSyncing from "./hooks/chat-syncing";
 import UiFeedback from "./hooks/ui-feedback";
 import LoadYTIframeAPI from './deps/yt-iframe-api'
 import createPlayer from './lib/player'
@@ -38,6 +39,7 @@ function initLiveview() {
     PresenceSyncing: PresenceSyncing(),
     UiFeedback: UiFeedback(),
     DragAndDropping: DragAndDropping(),
+    ChatSyncing: ChatSyncing(),
   }
 
   const liveSocket = new LiveSocket("/live", Socket, {
