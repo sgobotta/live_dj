e36952d3c4a7e6ee3dce0f75012df12d0c002800
Santiago <santiago@camba.coop>
2020-11-05 21:01:52 -0300

    :computer: Creates an Account Ecto schema

---
+++ lib/live_dj/organizer/account.ex
@@ -0,0 +1,21 @@
+defmodule LiveDj.Organizer.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "accounts" do
+    field :uuid, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @fields [:uuid, :username]
+
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, @fields)
+    |> validate_required([:uuid])
+    |> unique_constraint(:uuid)
+  end
+
+end
