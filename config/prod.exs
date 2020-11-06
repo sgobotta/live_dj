77187531906f48c1020b247df5cd525e39455078
Santiago <santiago@camba.coop>
2020-11-06 03:35:53 -0300

    :computer: Fixes typo on the prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :live_dj, LiveDjWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: {:system: "APP_HOST"}, port: 443],
+  url: [scheme: "https", host: {:system, "APP_HOST"}, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
