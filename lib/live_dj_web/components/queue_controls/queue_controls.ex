78f36ae20e2b2479584f3521e9112670f657f3b8
Santiago <santiago@camba.coop>
2020-11-09 17:23:05 -0300

    :computer: Updates queue control to implement a handle_event save queue

--- lib/live_dj_web/components/queue_controls/queue_controls.ex
+++ lib/live_dj_web/components/queue_controls/queue_controls.ex
@@ -5,10 +5,35 @@ defmodule LiveDjWeb.Components.QueueControls do
 
   use LiveDjWeb, :live_component
 
+  alias LiveDj.Organizer
+  alias LiveDj.Organizer.Queue
+
+  @impl true
   def update(assigns, socket) do
     {:ok,
       socket
       |> assign(assigns)
     }
+  end
+
+  @impl true
+  def handle_event("save_queue", _params, %{assigns: assigns} = socket) do
+    %{
+      room: room,
+      slug: slug,
+      video_queue: video_queue,
+      video_queue_controls: video_queue_controls
+    } = assigns
+    video_queue = Enum.map(video_queue, fn {v, _} -> v end)
+    {:ok, _room} = Organizer.update_room(room, %{queue: video_queue})
+
+    Phoenix.PubSub.broadcast(
+      LiveDj.PubSub,
+      "room:" <> slug,
+      {:save_queue,
+        %{video_queue_controls: Queue.mark_as_saved(video_queue_controls)}}
+    )
+
+    {:noreply, socket}
   end
 end
