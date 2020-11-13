2ab3576b093e9618b5f1ba87757477378ad8a337
Santiago <santiago@camba.coop>
2020-11-13 02:36:54 -0300

    :computer: Implements a loader component

---
+++ lib/live_dj_web/components/launcher_loader/launcher_loader.ex
@@ -0,0 +1,14 @@
+defmodule LiveDjWeb.Components.LauncherLoader do
+  @moduledoc """
+  Responsible for displaying a loader at launch time
+  """
+
+  use LiveDjWeb, :live_component
+
+  def update(assigns, socket) do
+    {:ok,
+      socket
+      |> assign(assigns)
+    }
+  end
+end
