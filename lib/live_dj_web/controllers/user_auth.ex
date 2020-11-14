5169232f1034ed9fde54e28a0f0aded6678abcd9
Santiago <santiago@camba.coop>
2020-11-14 17:12:57 -0300

    :computer: Updates the user auth controller

--- lib/live_dj_web/controllers/user_auth.ex
+++ lib/live_dj_web/controllers/user_auth.ex
@@ -1,5 +1,6 @@
 defmodule LiveDjWeb.UserAuth do
   import Plug.Conn
+  import Phoenix.Controller
 
   alias LiveDj.Accounts
 
@@ -21,12 +22,14 @@ defmodule LiveDjWeb.UserAuth do
   """
   def log_in_user(conn, user, params \\ %{}) do
     token = Accounts.generate_user_session_token(user)
+    user_return_to = get_session(conn, :user_return_to)
 
     conn
     |> renew_session()
     |> put_session(:user_token, token)
     |> put_session(:live_socket_id, "users_sessions:#{Base.url_encode64(token)}")
     |> maybe_write_remember_me_cookie(token, params)
+    |> redirect(to: user_return_to || signed_in_path(conn))
   end
 
   defp maybe_write_remember_me_cookie(conn, token, %{"remember_me" => "true"}) do
@@ -82,4 +85,19 @@ defmodule LiveDjWeb.UserAuth do
       end
     end
   end
+
+  @doc """
+  Used for routes that require the user to not be authenticated.
+  """
+  def redirect_if_user_is_authenticated(conn, _opts) do
+    if conn.assigns[:current_user] do
+      conn
+      |> redirect(to: signed_in_path(conn))
+      |> halt()
+    else
+      conn
+    end
+  end
+
+  defp signed_in_path(_conn), do: "/"
 end
