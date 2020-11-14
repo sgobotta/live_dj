d04c0f2ef33e314b96ccd78e1cc53938dacb8f9e
Santiago <santiago@camba.coop>
2020-11-14 04:24:54 -0300

    :computer: Updtes the accounts module with a session interface

--- lib/live_dj/accounts.ex
+++ lib/live_dj/accounts.ex
@@ -5,7 +5,7 @@ defmodule LiveDj.Accounts do
 
   import Ecto.Query, warn: false
   alias LiveDj.Repo
-  alias LiveDj.Accounts.{User}
+  alias LiveDj.Accounts.{User, UserToken}
 
   @doc """
   Registers a user.
@@ -36,5 +36,34 @@ defmodule LiveDj.Accounts do
   """
   def change_user_registration(%User{} = user, attrs \\ %{}) do
     User.registration_changeset(user, attrs)
+  end
+
+  ## Session
+
+  @doc """
+  Generates a session token.
+  """
+  def generate_user_session_token(user) do
+    {token, user_token} = UserToken.build_session_token(user)
+    Repo.insert!(user_token)
+    token
+  end
+
+  @doc """
+  Gets the user with the given signed token.
+  """
+  def get_user_by_session_token(nil), do: nil
+
+  def get_user_by_session_token(token) when is_binary(token) do
+    {:ok, query} = UserToken.verify_session_token_query(token)
+    Repo.one(query)
+  end
+
+  @doc """
+  Deletes the signed token with the given context.
+  """
+  def delete_session_token(token) do
+    Repo.delete_all(UserToken.token_and_context_query(token, "session"))
+    :ok
   end
 end
