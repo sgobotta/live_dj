7d405ad7a79c7117a5f2a943b5cbc9c5b1c49218
Santiago <santiago@camba.coop>
2020-11-14 01:25:59 -0300

    :computer: Implements an Accounts module

---
+++ lib/live_dj/accounts.ex
@@ -0,0 +1,40 @@
+defmodule LiveDj.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  import Ecto.Query, warn: false
+  alias LiveDj.Repo
+  alias LiveDj.Accounts.{User}
+
+  @doc """
+  Registers a user.
+
+  ## Examples
+
+      iex> register_user(%{field: value})
+      {:ok, %User{}}
+
+      iex> register_user(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def register_user(attrs) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user changes.
+
+  ## Examples
+
+      iex> change_user_registration(user)
+      %Ecto.Changeset{data: %User{}}
+
+  """
+  def change_user_registration(%User{} = user, attrs \\ %{}) do
+    User.registration_changeset(user, attrs)
+  end
+end
