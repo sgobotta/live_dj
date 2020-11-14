539f3d93d8cb4448369793764110ff918718b202
Santiago <santiago@camba.coop>
2020-11-14 17:10:22 -0300

    :computer: Implements a user registration controller

---
+++ lib/live_dj_web/controllers/user_registration_controller.ex
@@ -0,0 +1,25 @@
+defmodule LiveDjWeb.UserRegistrationController do
+  use LiveDjWeb, :controller
+
+  alias LiveDj.Accounts
+  alias LiveDj.Accounts.User
+  alias LiveDjWeb.UserAuth
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user_registration(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
+
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> UserAuth.log_in_user(user)
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
