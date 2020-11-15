a3a2697cd819500170a63a121918a501f24a1d05
Santiago <santiago@camba.coop>
2020-11-15 18:21:17 -0300

    :computer: Updates live registration component

--- lib/live_dj_web/components/settings/user_registration/user_registration.ex
+++ lib/live_dj_web/components/settings/user_registration/user_registration.ex
@@ -5,15 +5,19 @@ defmodule LiveDjWeb.Components.Settings.UserRegistration do
 
   use LiveDjWeb, :live_component
 
-  def update(assigns, conn) do
+  alias LiveDj.Accounts
+  alias LiveDj.Accounts.User
+
+  def update(assigns, socket) do
     {:ok,
-      conn
-      |> assign(:trigger_submit, true)
+      socket
+      |> assign(:trigger_submit, false)
       |> assign(assigns)
     }
   end
 
-  def handle_event("submit_changeset", user, socket) do
-    {:noreply, assign(socket, user_changeset: user)}
+  def handle_event("submit_changeset", %{"user" => user_params}, socket) do
+    changeset = Accounts.change_user_registration(%User{}, user_params)
+    {:noreply, assign(socket, user_changeset: changeset, trigger_submit: true)}
   end
 end
