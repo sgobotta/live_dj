6a55766aab3d576a75399fa653be40676dc9e58c
Santiago <santiago@camba.coop>
2020-11-15 16:19:42 -0300

    :computer: Implements username edition on live settings

--- lib/live_dj_web/components/settings/username_edit/username_edit.ex
+++ lib/live_dj_web/components/settings/username_edit/username_edit.ex
@@ -6,28 +6,41 @@ defmodule LiveDjWeb.Components.Settings.UsernameEdit do
   use LiveDjWeb, :live_component
 
   alias LiveDj.Accounts
-  alias LiveDj.Accounts.User
 
-  def update(assigns, conn) do
+  def update(assigns, socket) do
     {:ok,
-      conn
+      socket
       |> assign(assigns)
     }
   end
 
-  def handle_event("submit_changeset", %{"user" => user_params}, conn) do
-    case Accounts.register_user(user_params) do
+  def handle_event(
+    "submit_changeset",
+    %{"user" => user_params, "current_password" => password},
+    socket
+  ) do
+    %{current_user: current_user} = socket.assigns
+    user = current_user
+
+    case Accounts.apply_user_username(user, password, user_params) do
       {:ok, _user} ->
+        case Accounts.update_user_username(user, password, user_params) do
+          {:ok, user} ->
+            %{user: %{uuid: uuid}, room: %{slug: slug}} = socket.assigns
+            :ok = Phoenix.PubSub.broadcast(
+              LiveDj.PubSub,
+              "room:" <> slug,
+              {:username_changed, %{uuid: uuid, username: user.username}}
+            )
+            {:noreply,
+              socket
+              |> put_flash(:info, "Username updated successfully.")}
 
-        conn
-        |> put_flash(:info, "User created successfully.")
-        {:noreply, conn}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-
-        {:noreply,
-          conn
-          |> assign(:username_changeset, changeset)}
+          {:error, changeset} ->
+            {:noreply, assign(socket, :username_changeset, changeset)}
+        end
+      {:error, changeset} ->
+        {:noreply, assign(socket, :username_changeset, changeset)}
     end
   end
 end
