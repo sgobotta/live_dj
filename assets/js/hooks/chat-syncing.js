ab0451e72b1dacd5777f69bca3ffc2824c2f592e
Santiago <santiago@camba.coop>
2020-11-20 01:48:53 -0300

    :computer: Removes chat onmouse over listener

--- assets/js/hooks/chat-syncing.js
+++ assets/js/hooks/chat-syncing.js
@@ -4,12 +4,6 @@ const ChatSyncing = () => ({
     const chat = document.getElementById('chat')
     chat.scrollTop = chat.scrollHeight
 
-    const input = document.getElementById('submit_message')
-
-    input.onmouseenter = () => {
-      input.focus()
-    }
-
     this.handleEvent('receive_new_message', () => {
       chat.scrollTop = chat.scrollHeight
     })
