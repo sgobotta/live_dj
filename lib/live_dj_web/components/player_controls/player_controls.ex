f9690a9ac0e473085f4f1eaface2ad4852f90401
Santiago <santiago@camba.coop>
2020-11-21 03:06:39 -0300

    :computer: Moves the player_signal_play_next handler to the player_controls component

--- lib/live_dj_web/components/player_controls/player_controls.ex
+++ lib/live_dj_web/components/player_controls/player_controls.ex
@@ -16,6 +16,11 @@ defmodule LiveDjWeb.Components.PlayerControls do
     }
   end
 
+# ===========================================================================
+#
+# Very similar functions, refactor if not used for a specific task
+#
+
   def handle_event("player_signal_play_previous", _params, socket) do
     %{video_queue: video_queue, player: player} = socket.assigns
     %{video_id: current_video_id} = player
@@ -39,4 +44,31 @@ defmodule LiveDjWeb.Components.PlayerControls do
         {:noreply, socket}
     end
   end
+
+  def handle_event("player_signal_play_next", _params, socket) do
+    %{video_queue: video_queue, player: player} = socket.assigns
+    %{video_id: current_video_id} = player
+    video_queue = Enum.map(video_queue, fn {v, _} -> v end)
+    next_video = Queue.get_next_video(video_queue, current_video_id)
+
+    case next_video do
+      nil -> {:noreply, socket}
+      video ->
+        %{slug: slug} = socket.assigns
+        %{video_id: video_id} = video
+        player = Player.update(player, %{video_id: video_id, time: 0, state: "playing", previous_id: video.previous, next_id: video.next})
+        player_controls = Player.get_controls_state(player)
+
+        :ok = Phoenix.PubSub.broadcast(
+          LiveDj.PubSub,
+          "room:" <> slug,
+          {:player_signal_play_next, %{player: player, player_controls: player_controls}}
+        )
+
+        {:noreply, socket}
+    end
+  end
+
+#
+# ===========================================================================
 end
--- lib/live_dj_web/live/room/show_live.ex
+++ lib/live_dj_web/live/room/show_live.ex
@@ -421,38 +421,6 @@ defmodule LiveDjWeb.Room.ShowLive do
     end
   end
 
-# ===========================================================================
-#
-# Very similar functions, refactor if not used for a specific task
-#
-
-  def handle_event("player_signal_play_next", _params, socket) do
-    %{video_queue: video_queue, player: player} = socket.assigns
-    %{video_id: current_video_id} = player
-    video_queue = Enum.map(video_queue, fn {v, _} -> v end)
-    next_video = Queue.get_next_video(video_queue, current_video_id)
-
-    case next_video do
-      nil -> {:noreply, socket}
-      video ->
-        %{slug: slug} = socket.assigns
-        %{video_id: video_id} = video
-        player = Player.update(player, %{video_id: video_id, time: 0, state: "playing", previous_id: video.previous, next_id: video.next})
-        player_controls = Player.get_controls_state(player)
-
-        :ok = Phoenix.PubSub.broadcast(
-          LiveDj.PubSub,
-          "room:" <> slug,
-          {:player_signal_play_next, %{player: player, player_controls: player_controls}}
-        )
-
-        {:noreply, socket}
-    end
-  end
-
-#
-# ===========================================================================
-
   @impl true
   def handle_event("remove_track", params, socket) do
     :ok = Phoenix.PubSub.broadcast(
