402cf2630f1d26ba744a25f5290a7551e79700d3
Santiago <santiago@camba.coop>
2020-11-21 03:02:04 -0300

    :computer: Refactors the player_signal_play_previous handler to a player_controls component

--- lib/live_dj_web/components/player_controls/player_controls.ex
+++ lib/live_dj_web/components/player_controls/player_controls.ex
@@ -5,10 +5,38 @@ defmodule LiveDjWeb.Components.PlayerControls do
 
   use LiveDjWeb, :live_component
 
+  alias LiveDj.Organizer.Player
+  alias LiveDj.Organizer.Queue
+
+
   def update(assigns, socket) do
     {:ok,
       socket
       |> assign(assigns)
     }
+  end
+
+  def handle_event("player_signal_play_previous", _params, socket) do
+    %{video_queue: video_queue, player: player} = socket.assigns
+    %{video_id: current_video_id} = player
+    video_queue = Enum.map(video_queue, fn {v, _} -> v end)
+    previous_video = Queue.get_previous_video(video_queue, current_video_id)
+
+    case previous_video do
+      nil -> {:noreply, socket}
+      video ->
+        %{slug: slug} = socket.assigns
+        %{video_id: video_id} = video
+        player = Player.update(player, %{video_id: video_id, time: 0, state: "playing", previous_id: video.previous, next_id: video.next})
+        player_controls = Player.get_controls_state(player)
+
+        :ok = Phoenix.PubSub.broadcast(
+          LiveDj.PubSub,
+          "room:" <> slug,
+          {:player_signal_play_previous, %{player: player, player_controls: player_controls}}
+        )
+
+        {:noreply, socket}
+    end
   end
 end
