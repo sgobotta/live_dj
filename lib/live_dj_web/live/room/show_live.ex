11373125bc1ea3606adf0f4105549567e3845231
Santiago <santiago@camba.coop>
2020-11-23 16:30:27 -0300

    :computer: Integrates the volume controls component to the show view

--- lib/live_dj_web/live/room/show_live.ex
+++ lib/live_dj_web/live/room/show_live.ex
@@ -458,46 +458,6 @@ defmodule LiveDjWeb.Room.ShowLive do
   end
 
   @impl true
-  def handle_event("player_signal_toggle_volume", _params, socket) do
-    %{
-      volume_controls: volume_controls,
-      slug: slug,
-      user: %{uuid: uuid},
-    } = socket.assigns
-    %{is_muted: is_muted, volume_level: volume_level} = volume_controls
-
-    volume_level = case !is_muted do
-      true -> 0
-      false -> volume_level
-    end
-
-    volume_icon = VolumeControls.get_volume_icon(volume_level)
-
-    params = %{
-      is_muted: !is_muted,
-      volume_icon: volume_icon}
-    volume_controls = VolumeControls.update(volume_controls, params)
-    socket = assign(socket, :volume_controls, volume_controls)
-
-    :ok = Phoenix.PubSub.broadcast(
-      LiveDj.PubSub,
-      "room:" <> slug,
-      {:volume_level_changed, %{uuid: uuid, volume_level: volume_level, volume_icon: volume_icon}}
-    )
-
-    case is_muted do
-      true ->
-        {:noreply,
-          socket
-          |> push_event("receive_unmute_signal", %{})}
-      false ->
-        {:noreply,
-          socket
-          |> push_event("receive_mute_signal", %{})}
-    end
-  end
-
-  @impl true
   def handle_event("volume_level_changed", volume_level, socket) do
     %{slug: slug,
       user: %{uuid: uuid},
