26e635705999f9ab417524f104d8cf52a3f9cecd
Santiago <santiago@camba.coop>
2020-11-28 15:36:13 -0300

    :computer: new_live_view: updates handlers to receive rooms player and queue information from their socket
    + Code refactor for readability

--- lib/live_dj_web/live/room/new_live.ex
+++ lib/live_dj_web/live/room/new_live.ex
@@ -4,8 +4,9 @@ defmodule LiveDjWeb.Room.NewLive do
   alias LiveDj.Repo
   alias LiveDj.Organizer
   alias LiveDj.Organizer.Room
+  alias LiveDj.Organizer.Queue
 
-  @tick_rate :timer.seconds(10)
+  @tick_rate :timer.seconds(15)
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,11 +16,20 @@ defmodule LiveDjWeb.Room.NewLive do
     end
 
     public_rooms = Organizer.list_rooms()
-    viewers_quantity = for room <- public_rooms, do: {String.to_atom(room.title), Organizer.viewers_quantity(room)}
+
+    rooms_players = for room <- public_rooms do
+      {String.to_atom(room.slug), nil}
+    end
+    rooms_queues = for room <- public_rooms, do: {String.to_atom(room.slug), []}
+    viewers_quantity = for room <- public_rooms do
+      {String.to_atom(room.slug), Organizer.viewers_quantity(room)}
+    end
 
     socket =
       socket
       |> assign(:public_rooms, public_rooms)
+      |> assign(:rooms_players, rooms_players)
+      |> assign(:rooms_queues, rooms_queues)
       |> assign(:viewers_quantity, viewers_quantity)
       |> put_changeset()
 
@@ -34,39 +44,60 @@ defmodule LiveDjWeb.Room.NewLive do
       update(
         socket,
         :viewers_quantity,
-        fn _viewers_quantity -> for room <- socket.assigns.public_rooms, do: {String.to_atom(room.title), Organizer.viewers_quantity(room)} end
+        fn _viewers_quantity ->
+          for room <- socket.assigns.public_rooms do
+            {String.to_atom(room.slug), Organizer.viewers_quantity(room)}
+          end
+        end
       )
     {:noreply, socket}
   end
 
   def handle_info(:tick, socket) do
+    %{assigns: %{public_rooms: public_rooms}} = socket
     schedule_next_tick()
 
-    IO.inspect("tick")
-
-    slug = "vulfpeck"
-
-    Organizer.subscribe(:request_current_player, slug)
-
-    :ok = Phoenix.PubSub.broadcast_from(
-      LiveDj.PubSub,
-      self(),
-      "room:" <> slug,
-      {:request_current_player, %{}}
-    )
+    request_rooms_player(public_rooms)
 
     {:noreply, socket}
   end
 
   def handle_info({:receive_current_player, %{slug: slug} = params}, socket) do
     Organizer.unsubscribe(:request_current_player, slug)
-    %{player: player} = params
-    IO.inspect("slug")
-    IO.inspect(slug)
-    IO.inspect("Receives player")
-    IO.inspect(player)
+    %{assigns: %{
+      rooms_players: rooms_players, rooms_queues: rooms_queues}} = socket
 
-    {:noreply, socket}
+    current_slug = String.to_atom(slug)
+    %{player: player, video_queue: video_queue} = params
+
+    rooms_players = case player.state do
+      "stopped" ->
+        rooms_players
+      _ ->
+        current_track = Enum.map(video_queue, fn {v, _} -> v end)
+        |> Queue.get_video_by_id(player.video_id)
+
+        for player <- rooms_players do
+          {slug, player} = player
+          case slug do
+            ^current_slug -> {current_slug, current_track}
+            slug -> {slug, player}
+          end
+        end
+    end
+
+    rooms_queues = for queue <- rooms_queues do
+      {slug, queue} = queue
+      case slug do
+        ^current_slug -> {current_slug, video_queue}
+        slug -> {slug, queue}
+      end
+    end
+
+    {:noreply,
+      socket
+      |> assign(:rooms_players, rooms_players)
+      |> assign(:rooms_queues, rooms_queues)}
   end
 
   @impl true
@@ -106,5 +137,19 @@ defmodule LiveDjWeb.Room.NewLive do
 
   defp schedule_next_tick() do
     Process.send_after(self(), :tick, @tick_rate)
+  end
+
+  defp request_rooms_player(rooms) do
+
+    for room <- rooms do
+      %{slug: slug} = room
+      Organizer.subscribe(:request_current_player, slug)
+      :ok = Phoenix.PubSub.broadcast_from(
+        LiveDj.PubSub,
+        self(),
+        "room:" <> slug,
+        {:request_current_player, %{}}
+      )
+    end
   end
 end
