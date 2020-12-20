62835a2a959c085333e35320b9f6775b11f9f806
Santiago <santiago@camba.coop>
2020-12-20 12:45:18 -0300

    :computer: Implements a donations controller

--- lib/live_dj_web/controllers/donations_controller.ex
+++ lib/live_dj_web/controllers/donations_controller.ex
@@ -4,4 +4,19 @@ defmodule LiveDjWeb.DonationsController do
   def index(conn, _params)   do
     render(conn, "index.html")
   end
+
+  def new(conn, _params) do
+    conn
+      |> redirect(to: "/donations/thanks")
+  end
+
+  def thanks(conn, _params) do
+    %{assigns: %{current_user: current_user, visitor: visitor}} = conn
+    case visitor do
+      true ->
+        render(conn, "new.html", username: "Guest")
+      false ->
+        render(conn, "new.html", username: current_user.username)
+    end
+  end
 end
