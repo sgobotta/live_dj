52cf2dc3732091553f1c58fb56a469a2a01b5df2
Santiago <santiago@camba.coop>
2020-12-20 17:16:20 -0300

    :computer: Updates the donations controller and controller tests

--- lib/live_dj_web/controllers/donations_controller.ex
+++ lib/live_dj_web/controllers/donations_controller.ex
@@ -1,16 +1,46 @@
 defmodule LiveDjWeb.DonationsController do
   use LiveDjWeb, :controller
 
+  alias LiveDj.Accounts
+  alias LiveDj.Payments
+
   def index(conn, _params)   do
     render(conn, "index.html")
   end
 
-  def new(conn, _params) do
-    conn
-      |> redirect(to: "/donations/thanks")
+  def new(conn, params) do
+    %{"donation_id" => donation_id} = params
+    %{assigns: %{current_user: current_user, visitor: visitor}} = conn
+
+    case Payments.get_plan_by_plan_id(donation_id) do
+      nil ->
+        conn
+          |> redirect(to: "/")
+      plan ->
+        IO.inspect("plan")
+        IO.inspect(plan)
+
+        # case Payments.create_order(user_params) do
+        #   {:ok, order} ->
+        #     IO.inspect("ORDER")
+        #     IO.inspect(order)
+        #     {:ok, _} =
+        #       Payments.deliver_payment_confirmation(
+        #         user,
+        #         &Routes.user_confirmation_url(conn, :confirm, &1)
+        #       )
+        #   {:error, %Ecto.Changeset{} = changeset} ->
+        #     render(conn, "new.html", changeset: changeset)
+        # end
+
+
+        conn
+          |> redirect(to: "/donations/thanks")
+    end
+
   end
 
-  def thanks(conn, _params) do
+  def thanks(conn, params) do
     %{assigns: %{current_user: current_user, visitor: visitor}} = conn
     case visitor do
       true ->
---
+++ test/live_dj_web/controllers/donations_controller_test.exs
@@ -0,0 +1,47 @@
+defmodule LiveDjWeb.DonationsControllerTest do
+  use LiveDjWeb.ConnCase, async: true
+
+  alias LiveDj.Accounts
+  alias LiveDjWeb.UserAuth
+  import LiveDj.AccountsFixtures
+  import LiveDj.PaymentFixtures
+
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> Map.replace!(:secret_key_base, LiveDjWeb.Endpoint.config(:secret_key_base))
+      |> init_test_session(%{})
+
+    plan_fixture()
+    %{user: user_fixture(), conn: conn}
+  end
+
+  describe "GET /donations/:donation_id" do
+    test "receives a valid donation as a visitor user", %{conn: conn, user: user} do
+      conn = get(conn, Routes.donations_path(conn, :new, "123"))
+      response = html_response(conn, 302)
+      assert redirected_to(conn) == "/donations/thanks"
+    end
+
+    test "receives an invalid donation as a visitor user", %{conn: conn, user: user} do
+      conn = get(conn, Routes.donations_path(conn, :new, "456"))
+      response = html_response(conn, 302)
+      assert redirected_to(conn) == "/"
+    end
+
+    test "receives a valid donation as a registered user", %{conn: conn, user: user} do
+      conn = log_in_user(conn, user)
+        |> get(Routes.donations_path(conn, :new, "123"))
+      response = html_response(conn, 302)
+      assert redirected_to(conn) == "/donations/thanks"
+    end
+
+    test "receives an invalid donation as a registered user", %{conn: conn, user: user} do
+      conn = log_in_user(conn, user)
+        |> get(Routes.donations_path(conn, :new, "456"))
+      response = html_response(conn, 302)
+      assert redirected_to(conn) == "/"
+    end
+
+  end
+end
