d055215c18e6f23b7f75852afd00ac0db9cd68a9
Santiago <santiago@camba.coop>
2020-12-21 14:17:18 -0300

    :test_tube: Donations Controller Tests: refactors setup params

--- test/live_dj_web/controllers/donations_controller_test.exs
+++ test/live_dj_web/controllers/donations_controller_test.exs
@@ -12,15 +12,18 @@ defmodule LiveDjWeb.DonationsControllerTest do
       |> Map.replace!(:secret_key_base, LiveDjWeb.Endpoint.config(:secret_key_base))
       |> init_test_session(%{})
 
-    plan_fixture()
-    %{user: user_fixture(), conn: conn}
+    %{user: user_fixture(), conn: conn, plan: plan_fixture()}
   end
 
   describe "GET /donations/:donation_id" do
-    test "receives a valid donation as a visitor user", %{conn: conn, user: user} do
-      conn = get(conn, Routes.donations_path(conn, :new, "123"))
+    test "receives a valid donation as a visitor user", %{conn: conn, plan: plan} do
+      conn = get(conn, Routes.donations_path(conn, :new, plan.plan_id))
       response = html_response(conn, 302)
-      assert redirected_to(conn) == "/donations/thanks"
+      assert "/donations/thanks" = redir_path = redirected_to(conn)
+      conn = get(recycle(conn), redir_path)
+      response = html_response(conn, 200)
+      assert response =~ "Thank you, Guest!"
+      assert response =~ "Your donation has been successfully processed. Contributions let us improve our efforts to make this site a better experience for you!"
     end
 
     test "receives an invalid donation as a visitor user", %{conn: conn, user: user} do
@@ -29,11 +32,15 @@ defmodule LiveDjWeb.DonationsControllerTest do
       assert redirected_to(conn) == "/"
     end
 
-    test "receives a valid donation as a registered user", %{conn: conn, user: user} do
+    test "receives a valid donation as a registered user", %{conn: conn, plan: plan, user: user} do
       conn = log_in_user(conn, user)
-        |> get(Routes.donations_path(conn, :new, "123"))
+        |> get(Routes.donations_path(conn, :new, plan.plan_id))
       response = html_response(conn, 302)
-      assert redirected_to(conn) == "/donations/thanks"
+      assert "/donations/thanks" = redir_path = redirected_to(conn)
+      conn = get(recycle(conn), redir_path)
+      response = html_response(conn, 200)
+      assert response =~ "Thank you, #{user.username}!"
+      assert response =~ "Your donation has been successfully processed. Contributions let us improve our efforts to make this site a better experience for you!"
     end
 
     test "receives an invalid donation as a registered user", %{conn: conn, user: user} do
