373af18620f3d728f20d1bbd44eae1f3ac6d4a62
Santiago <santiago@camba.coop>
2020-12-22 17:04:09 -0300

    :computer: Updates plans schema, generates migration

--- lib/live_dj/payments/plan.ex
+++ lib/live_dj/payments/plan.ex
@@ -8,6 +8,7 @@ defmodule LiveDj.Payments.Plan do
     field :name, :string
     field :plan_id, :string
     field :type, :string
+    field :extra, {:array, :map}, default: []
 
     timestamps()
   end
@@ -15,7 +16,7 @@ defmodule LiveDj.Payments.Plan do
   @doc false
   def changeset(plan, attrs) do
     plan
-    |> cast(attrs, [:gateway, :plan_id, :amount, :type, :name])
+    |> cast(attrs, [:gateway, :plan_id, :amount, :type, :name, :extra])
     |> validate_required([:gateway, :plan_id, :amount, :type, :name])
   end
 end
