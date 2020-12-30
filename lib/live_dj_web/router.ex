c4a11b0f207095c26c120fb532746d3fef35b890
Santiago <sgobotta@gmail.com>
2020-12-30 03:17:28 -0300

    :computer: Router: Adds a settings route for payments

--- lib/live_dj_web/router.ex
+++ lib/live_dj_web/router.ex
@@ -69,6 +69,7 @@ defmodule LiveDjWeb.Router do
 
     get "/users/settings", UserSettingsController, :index
     get "/users/settings/account", UserSettingsController, :edit
+    get "/users/settings/payments", UserSettingsController, :show_payments
     put "/users/settings/update_username", UserSettingsController, :update_username
     put "/users/settings/update_password", UserSettingsController, :update_password
     put "/users/settings/update_email", UserSettingsController, :update_email
