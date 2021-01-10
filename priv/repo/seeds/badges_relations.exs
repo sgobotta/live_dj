416b14153302d0373b4f787e24880a80e55eb19b
Santiago <sgobotta@gmail.com>
2021-01-10 10:16:43 -0300

    :wrench: Slightly modifies badge_relations seeds

--- priv/repo/seeds/badges_relations.exs
+++ priv/repo/seeds/badges_relations.exs
@@ -3,8 +3,8 @@ alias LiveDj.Stats
 try do
   badges_users = [
     {1, "users-confirmed_via_link"},
-    {1, "payments-donate_once"},
-    {2, "payments-donate_once"}
+    {1, "rooms-create_once"},
+    {2, "rooms-create_once"}
   ]
   |> Enum.map(fn {user_id, badge_reference_name} ->
     :ok = Stats.assoc_user_badge(user_id, badge_reference_name)
