9ee1612de7b3dc1bcd6388c513401a9b483d27ac
Santiago <sgobotta@gmail.com>
2021-01-10 09:06:46 -0300

    :wrench: Updates badge-user relations seeds

--- priv/repo/seeds/badges_relations.exs
+++ priv/repo/seeds/badges_relations.exs
@@ -1,9 +1,13 @@
 alias LiveDj.Stats
 
 try do
-  badges_users = [{1, 1}, {1, 2}, {2,2}]
-  |> Enum.map(fn {user_id, badge_id} ->
-    :ok = Stats.assoc_user_badge(user_id, badge_id)
+  badges_users = [
+    {1, "users-confirmed_via_link"},
+    {1, "payments-donate_once"},
+    {2, "payments-donate_once"}
+  ]
+  |> Enum.map(fn {user_id, badge_reference_name} ->
+    :ok = Stats.assoc_user_badge(user_id, badge_reference_name)
   end)
 
   IO.inspect("Inserted #{length(badges_users)} user/badge relationships.")
