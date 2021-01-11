19b58dda90325836254494797978b9f21c9fb31e
Santiago <sgobotta@gmail.com>
2021-01-11 18:40:55 -0300

    :wrench: Updates UserRoom migration

--- priv/repo/migrations/20210110232916_create_users_rooms.exs
+++ priv/repo/migrations/20210110232916_create_users_rooms.exs
@@ -3,13 +3,14 @@ defmodule LiveDj.Repo.Migrations.CreateUsersRooms do
 
   def change do
     create table(:users_rooms) do
-      add :is_owner, :boolean, default: false
-      add :user_id, references(:users)
-      add :room_id, references(:rooms)
+      add :is_owner, :boolean, default: false, null: false
+      add :user_id, references(:users, on_delete: :nothing)
+      add :room_id, references(:rooms, on_delete: :nothing)
 
-      timestamps([{:updated_at, false}])
+      timestamps()
     end
 
-    create unique_index(:users_rooms, [:user_id, :room_id])
+    create index(:users_rooms, [:user_id])
+    create index(:users_rooms, [:room_id])
   end
 end
