60d18a0302591b1e60215117a96f0f3c4fca85a4
Santiago <sgobotta@gmail.com>
2021-01-11 18:40:21 -0300

    :computer: Updates UserRoom schema

--- lib/live_dj/accounts/user_room.ex
+++ lib/live_dj/accounts/user_room.ex
@@ -2,21 +2,18 @@ defmodule LiveDj.Accounts.UserRoom do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias LiveDj.Accounts.User
-  alias LiveDj.Organizer.Room
-
   schema "users_rooms" do
     field :is_owner, :boolean, default: false
-    belongs_to :user, User
-    belongs_to :room, Room
+    belongs_to :user, LiveDj.Accounts.User
+    belongs_to :room, LiveDj.Organizer.Room
 
-    timestamps([{:updated_at, false}])
+    timestamps()
   end
 
   @doc false
   def changeset(user_room, attrs) do
     user_room
-    |> cast(attrs, [:user_id, :room_id])
-    |> validate_required([:user_id, :room_id])
+    |> cast(attrs, [:user_id, :room_id, :is_owner])
+    |> validate_required([:user_id, :room_id, :is_owner])
   end
 end
