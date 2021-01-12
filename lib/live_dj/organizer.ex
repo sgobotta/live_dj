433ed2ab290ebab287f137401da173817f7d4384
Santiago <sgobotta@gmail.com>
2021-01-11 22:20:21 -0300

    :computer: Organizer: creates functions around the UserRoom schema

--- lib/live_dj/organizer.ex
+++ lib/live_dj/organizer.ex
@@ -160,4 +160,100 @@ defmodule LiveDj.Organizer do
   def viewers_quantity(room) do
     list_present(room.slug) |> length()
   end
+
+  alias LiveDj.Organizer.UserRoom
+
+  @doc """
+  Returns the list of users_rooms.
+
+  ## Examples
+
+      iex> list_users_rooms()
+      [%UserRoom{}, ...]
+
+  """
+  def list_users_rooms do
+    Repo.all(UserRoom)
+  end
+
+  @doc """
+  Gets a single user_room.
+
+  Raises `Ecto.NoResultsError` if the User room does not exist.
+
+  ## Examples
+
+      iex> get_user_room!(123)
+      %UserRoom{}
+
+      iex> get_user_room!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user_room!(id), do: Repo.get!(UserRoom, id)
+
+  @doc """
+  Creates a user_room.
+
+  ## Examples
+
+      iex> create_user_room(%{field: value})
+      {:ok, %UserRoom{}}
+
+      iex> create_user_room(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_user_room(attrs \\ %{}) do
+    %UserRoom{}
+    |> UserRoom.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a user_room.
+
+  ## Examples
+
+      iex> update_user_room(user_room, %{field: new_value})
+      {:ok, %UserRoom{}}
+
+      iex> update_user_room(user_room, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_user_room(%UserRoom{} = user_room, attrs) do
+    user_room
+    |> UserRoom.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a user_room.
+
+  ## Examples
+
+      iex> delete_user_room(user_room)
+      {:ok, %UserRoom{}}
+
+      iex> delete_user_room(user_room)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_user_room(%UserRoom{} = user_room) do
+    Repo.delete(user_room)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user_room changes.
+
+  ## Examples
+
+      iex> change_user_room(user_room)
+      %Ecto.Changeset{data: %UserRoom{}}
+
+  """
+  def change_user_room(%UserRoom{} = user_room, attrs \\ %{}) do
+    UserRoom.changeset(user_room, attrs)
+  end
 end
