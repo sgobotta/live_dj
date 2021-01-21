094e9702d14121c841e7c3356d9fe5879fe2c980
Santiago <sgobotta@gmail.com>
2021-01-20 21:14:59 -0300

    :wrench: Badges Seeds: small refactor to re-use seeds fixture in tests

--- priv/repo/seeds/badges.exs
+++ priv/repo/seeds/badges.exs
@@ -1,18 +1,18 @@
 alias LiveDj.Repo
-alias LiveDj.Seeds.Utils
 alias LiveDj.Stats.Badge
 
 try do
   badges = System.get_env("LIVEDJ_BADGES")
     |> Poison.decode!()
     |> Enum.map(fn badge ->
+      {:ok, date_time} = Ecto.Type.cast(:naive_datetime, badge["inserted_at"])
       %Badge{
         description: badge["description"],
         icon: badge["icon"],
         name: badge["name"],
         reference_name: badge["reference_name"],
-        inserted_at: Utils.date_to_naive_datetime(badge["inserted_at"]),
-        updated_at: Utils.date_to_naive_datetime(badge["inserted_at"])
+        inserted_at: date_time,
+        updated_at: date_time
       }
       |> Repo.insert()
     end)
