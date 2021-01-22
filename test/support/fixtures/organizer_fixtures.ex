c2051e413fc893625652c76f307baf6f65382165
Santiago <sgobotta@gmail.com>
2021-01-22 08:33:35 -0300

    :wrench: Implements a user_room fixture

--- test/support/fixtures/organizer_fixtures.ex
+++ test/support/fixtures/organizer_fixtures.ex
@@ -4,6 +4,8 @@ defmodule LiveDj.OrganizerFixtures do
   entities via the `LiveDj.Organizer` context.
   """
 
+  alias LiveDj.AccountsFixtures
+
   def room_fixture(attrs \\ %{}) do
     random_words = Enum.join(Faker.Lorem.words(5), " ")
     {:ok, room} =
@@ -14,5 +16,19 @@ defmodule LiveDj.OrganizerFixtures do
       })
       |> LiveDj.Organizer.create_room()
       room
+  end
+
+  def user_room_fixture(attrs \\ %{}) do
+    user = AccountsFixtures.user_fixture()
+    room = room_fixture()
+    {:ok, user_room} =
+      attrs
+      |> Enum.into(%{
+        is_owner: true,
+        room_id: room.id,
+        user_id: user.id,
+      })
+      |> LiveDj.Organizer.create_room()
+    %{room: room, user: user, user_room: user_room}
   end
 end
