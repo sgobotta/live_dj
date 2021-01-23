911884950b7ff2df082a5bead53f253fc443d335
Santiago <sgobotta@gmail.com>
2021-01-23 09:28:18 -0300

    :computer: Implements a permissions_groups relationship schema

---
+++ lib/live_dj/accounts/permission_group.ex
@@ -0,0 +1,18 @@
+defmodule LiveDj.Accounts.PermissionGroup do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "permissions_groups" do
+    belongs_to :permission, LiveDj.Accounts.Permission
+    belongs_to :group, LiveDj.Accounts.Group
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(permission_group, attrs) do
+    permission_group
+    |> cast(attrs, [:permission_id, :group_id])
+    |> validate_required([:permission_id, :group_id])
+  end
+end
