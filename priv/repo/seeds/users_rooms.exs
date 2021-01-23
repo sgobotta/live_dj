12c0284ec212c1f53d26d41fad4378516ffc1a67
Santiago <sgobotta@gmail.com>
2021-01-23 16:09:28 -0300

    :wrench: UserRoom: adds group_id assignation on users_rooms seeds

--- priv/repo/seeds/users_rooms.exs
+++ priv/repo/seeds/users_rooms.exs
@@ -2,47 +2,49 @@ alias LiveDj.Organizer
 
 try do
   users_rooms = [
+  # Assumes Rooms, Users and Groups were created
   # {room_id, user_id, is_owner}
-    {1,  1, true},
-    {1,  2, false},
-    {1,  3, false},
-    {1,  4, false},
-    {1,  5, false},
-    {1,  6, false},
+    {1,  1, true,  1},
+    {1,  2, false, 2},
+    {1,  3, false, 2},
+    {1,  4, false, 2},
+    {1,  5, false, 2},
+    {1,  6, false, 2},
 
-    {2,  1, true},
-    {2,  2, false},
-    {2,  3, false},
-    {2,  4, false},
-    {2,  5, false},
-    {2,  6, false},
+    {2,  1, true,  1},
+    {2,  2, false, 2},
+    {2,  3, false, 2},
+    {2,  4, false, 2},
+    {2,  5, false, 2},
+    {2,  6, false, 2},
 
-    {3,  1, true},
-    {3,  2, false},
-    {3,  3, false},
-    {3,  4, false},
-    {3,  5, false},
-    {3,  6, false},
+    {3,  1, true,  1},
+    {3,  2, false, 2},
+    {3,  3, false, 2},
+    {3,  4, false, 2},
+    {3,  5, false, 2},
+    {3,  6, false, 2},
 
-    {4,  1, true},
-    {5,  1, true},
-    {6,  6, true},
-    {7,  1, true},
-    {8,  1, true},
-    {9,  1, true},
-    {10, 1, true},
-    {11, 2, true},
-    {12, 6, true},
-    {13, 5, true},
-    {14, 1, true},
-    {15, 3, true},
+    {4,  1, true, 1},
+    {5,  1, true, 1},
+    {6,  6, true, 1},
+    {7,  1, true, 1},
+    {8,  1, true, 1},
+    {9,  1, true, 1},
+    {10, 1, true, 1},
+    {11, 2, true, 1},
+    {12, 6, true, 1},
+    {13, 5, true, 1},
+    {14, 1, true, 1},
+    {15, 3, true, 1},
   ]
   |> Enum.map(
-      fn {room_id, user_id, is_owner} ->
+      fn {room_id, user_id, is_owner, group_id} ->
         {:ok, _} = Organizer.create_user_room(%{
           is_owner: is_owner,
           room_id: room_id,
-          user_id: user_id
+          user_id: user_id,
+          group_id: group_id,
         })
       end
     )
