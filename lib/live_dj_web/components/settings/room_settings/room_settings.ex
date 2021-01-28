61a8c0130d117212aae990b130b3e6d21cc67cb7
Santiago <sgobotta@gmail.com>
2021-01-28 09:22:06 -0300

    :computer: Room Settings: Implements a live component

---
+++ lib/live_dj_web/components/settings/room_settings/room_settings.ex
@@ -0,0 +1,48 @@
+defmodule LiveDjWeb.Components.Settings.RoomSettings do
+  @moduledoc """
+  Responsible for displaying the room settings
+  """
+
+  use LiveDjWeb, :live_component
+
+  alias LiveDj.Organizer.Room
+  alias LiveDj.Repo
+
+  def update(assigns, socket) do
+    {:ok,
+      socket
+      |> assign(:changeset, assigns.room_changeset)
+      |> assign(:slug, assigns.room_changeset.data.slug)
+    }
+  end
+
+  def handle_event("validate", %{"room" => room_params}, socket) do
+    {:noreply,
+      socket
+      |> assign_changeset(socket.assigns.changeset, room_params)}
+  end
+
+  def handle_event("submit_changeset", _, socket) do
+    %{assigns: %{changeset: changeset}} = socket
+    case Repo.update(changeset) do
+      {:ok, room} ->
+        :ok = Phoenix.PubSub.broadcast(
+          LiveDj.PubSub,
+          "room:" <> room.slug,
+          {:update_socket, %{room: room}}
+        )
+        {:noreply,
+          socket
+          |> put_flash(:info, "Room updated succesfully!")}
+      {:error, changeset} ->
+        {:noreply,
+          socket
+          |> assign(:changeset, changeset)
+          |> put_flash(:error, "Could not save the room.")}
+    end
+  end
+
+  defp assign_changeset(socket, changeset, params) do
+    assign(socket, :changeset, Room.changeset(changeset, params))
+  end
+end
