2f1b80a354e5f2ae2cb9891b0d5a361c1dbcea43
Santiago <sgobotta@gmail.com>
2021-01-30 10:05:28 -0300

    :test_tube: ShowLive tests: Implements group assignation tests

--- test/live_dj_web/live/room/show_room_test.exs
+++ test/live_dj_web/live/room/show_room_test.exs
@@ -6,7 +6,7 @@ defmodule LiveDjWeb.ShowRoomTest do
   import LiveDj.AccountsFixtures
   import LiveDj.OrganizerFixtures
 
-  describe "ShowLive user room groups Assignation" do
+  describe "ShowLive user room groups assignation" do
 
     setup(%{conn: conn}) do
       # Creates an initial group
@@ -17,22 +17,42 @@ defmodule LiveDjWeb.ShowRoomTest do
         permissions: permissions,
         group_id: group.id
       })
+
+      %{conn: conn, group: group |> Repo.preload([:permissions])}
+    end
+
+    test "As a registered User When I belong to a group associated to a room I obtain those group permissions",
+      %{conn: conn, group: group}
+    do
       # Associates a group id to a new user for a new room
       %{room: room, user: user, user_room: _user_room} = user_room_fixture(%{
         is_owner: false, group_id: group.id
       })
-
       conn = log_in_user(conn, user)
 
-      %{conn: conn, group: group |> Repo.preload([:permissions]), room: room}
-    end
-
-    test "As a registered User When I belong to a certain group associated to a room I obtain those group permissions",
-      %{conn: conn, group: group, room: room}
-    do
       %{assigns: assigns} = _conn = get(conn, "/room/#{room.slug}")
 
-      assert group.permissions == assigns.user_room_group.permissions
+      refute assigns.visitor
+      assert assigns.user_room_group.permissions == group.permissions
+    end
+
+    test "As a registered User When I don't belong to a group associated to a room I obtain no permissions",
+      %{conn: conn}
+    do
+      %{conn: conn, user: _user} = register_and_log_in_user(%{conn: conn})
+      room = room_fixture()
+      %{assigns: assigns} = _conn = get(conn, "/room/#{room.slug}")
+
+      refute assigns.visitor
+      assert assigns.user_room_group.permissions == []
+    end
+
+    test "As a visitor User I obtain no permissions", %{conn: conn} do
+      room = room_fixture()
+      %{assigns: assigns} = _conn = get(conn, "/room/#{room.slug}")
+
+      assert assigns.visitor
+      assert assigns.user_room_group.permissions == []
     end
   end
 end
