4064edd9e3c48d2daac2655c9382546592604416
Santiago <sgobotta@gmail.com>
2021-01-31 18:20:44 -0300

    :computer: Player Controls: applies permissions to handle ui effects

--- lib/live_dj_web/components/player_controls/player_controls.ex
+++ lib/live_dj_web/components/player_controls/player_controls.ex
@@ -9,18 +9,41 @@ defmodule LiveDjWeb.Components.PlayerControls do
   alias LiveDj.Organizer.Queue
 
   def update(assigns, socket) do
+
+    %{user_room_group: %{permissions: permissions}} = assigns
+
+    player_permissions = %{
+      can_play_track:  has_permission(permissions, "can_play_track"),
+      can_pause_track: has_permission(permissions, "can_pause_track"),
+      can_play_next_track: has_permission(permissions, "can_play_next_track"),
+      can_play_previous_track: has_permission(permissions, "can_play_previous_track"),
+    }
+
     {:ok,
       socket
+      |> assign(:player_permissions, player_permissions)
       |> assign(assigns)
     }
   end
 
-  def handle_event("player_signal_playing", _params, socket) do
+  defp has_permission(permissions, permission) do
+    Enum.any?(permissions, fn p -> p.codename == permission end)
+  end
+
+  def handle_event("player_signal_playing", _params,
+    %{assigns: %{player_permissions: %{can_play_track: true}}} = socket
+  ) do
     :ok = Phoenix.PubSub.broadcast(
       LiveDj.PubSub,
       "room:" <> socket.assigns.slug,
       {:player_signal_playing, %{state: "playing"}}
     )
+    {:noreply, socket}
+  end
+
+  def handle_event("player_signal_playing", _params,
+    %{assigns: %{player_permissions: %{can_play_track: false}}} = socket
+  ) do
     {:noreply, socket}
   end
 
@@ -89,29 +112,33 @@ defmodule LiveDjWeb.Components.PlayerControls do
 #
 # ===========================================================================
 
-  def render_player_control_button("play-controls", player_state, player_controls, assigns) do
+  def render_player_control_button(
+    "play-controls", player_state, player_controls,
+    {can_play_track, can_pause_track},
+    assigns
+  ) do
     %{
       pause_button_state: pause_button_state,
       play_button_state: play_button_state
     } = player_controls
     is_player_loading = pause_button_state == "disabled" && play_button_state == "disabled"
-    play_button_class = case is_player_loading do
-      true -> "show_loader_control_button player-control-spin"
+    {play_button_class, anchor_class} = case is_player_loading do
+      true -> {"show_loader_control_button player-control-spin", ""}
       false -> case player_state do
-        "playing" -> "show_pause_control_button"
-        "stopped" -> "show_play_control_button"
-        "paused" ->  "show_play_control_button"
+        "paused" ->  {"show_play_control_button",  if can_play_track  do "" else "disabled" end}
+        "stopped" -> {"show_play_control_button",  if can_play_track  do "" else "disabled" end}
+        "playing" -> {"show_pause_control_button", if can_pause_track do "" else "disabled" end}
       end
     end
     player_event = case player_state do
-      "paused"  -> "player_signal_playing"
-      "stopped" -> "player_signal_playing"
-      "playing" -> "player_signal_paused"
+      "paused"  -> if can_play_track do "player_signal_playing" else "" end
+      "stopped" -> if can_play_track do "player_signal_playing" else "" end
+      "playing" -> if can_pause_track do "player_signal_paused" else "" end
       _         -> ""
     end
 
     ~L"""
-      <a phx-click="<%= player_event %>" phx-target="<%= assigns %>">
+      <a class="<%= anchor_class %>" phx-click="<%= player_event %>" phx-target="<%= assigns %>">
         <%= render_svg(
           "icons/player/play-controls",
           "h-12 w-12 player-control clickeable #{play_button_class}"
@@ -120,17 +147,21 @@ defmodule LiveDjWeb.Components.PlayerControls do
     """
   end
 
-  def render_player_control_button(button, event_name, button_state, assigns) do
+  def render_player_control_button(button, event_name, button_state, has_permission, assigns) do
     %{link_props: link_props, svg_classes: svg_classes} = case button_state do
       "disabled" ->
         %{
           link_props: %{class: "disabled", phx_click: "", phx_target: nil},
-          svg_classes: ""
+          svg_classes: "",
         }
       _ ->
         %{
-          link_props: %{class: "", phx_click: event_name, phx_target: assigns},
-          svg_classes: "player-control clickeable"
+          link_props: %{
+            class: if has_permission do "" else "disabled" end,
+            phx_click: if has_permission do event_name else "" end,
+            phx_target: if has_permission do assigns else nil end,
+          },
+          svg_classes: "player-control clickeable",
         }
     end
 
