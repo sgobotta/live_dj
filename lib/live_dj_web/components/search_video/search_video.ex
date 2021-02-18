c435af5d383a35dd96a2577f4575b156cf4f5039
Santiago <sgobotta@gmail.com>
2021-02-18 19:51:32 -0300

    :computer: SearchVideo: adds phx-change event on video search

--- lib/live_dj_web/components/search_video/search_video.ex
+++ lib/live_dj_web/components/search_video/search_video.ex
@@ -7,21 +7,21 @@ defmodule LiveDjWeb.Components.SearchVideo do
 
   alias LiveDj.Organizer.{Queue, Video}
 
+  @impl true
   def update(assigns, socket) do
     {:ok,
       socket
+      |> assign(:search_query, "")
       |> assign(assigns)
     }
   end
 
   @impl true
-  def handle_event(
-    "search",
-    %{"search_field" => %{"query" => query}},
-    %{assigns: %{video_queue: video_queue}} = socket
+  def handle_event("submit", _params,
+    %{assigns: %{video_queue: video_queue, search_query: query}} = socket
   ) do
     video_queue = Enum.map(video_queue, fn {v, _} -> v end)
-    opts = [maxResults: 25]
+    opts = [maxResults: 5]
     {:ok, search_result, _pag_opts} = Tubex.Video.search_by_query(query, opts)
     search_result = Enum.map(search_result, fn search ->
       video = Video.from_tubex_video(search)
@@ -29,22 +29,32 @@ defmodule LiveDjWeb.Components.SearchVideo do
       Video.update(video, %{is_queued: is_queued}) end)
     {:noreply,
       socket
-      |> assign(:search_result, search_result)
+      |> assign(:search_result, Enum.with_index(search_result))
       |> push_event("receive_search_completed_signal", %{})}
   end
 
+  @impl true
+  def handle_event(
+    "search",
+    %{"search_field" => %{"query" => query}},
+    %{assigns: %{search_query: search_query}} = socket
+  ) do
+    {:noreply, assign(socket, :search_query, query)}
+  end
 
   @impl true
   def handle_event("add_to_queue", selected_video, socket) do
     %{assigns: %{search_result: search_result, video_queue: video_queue,
       user: user}} = socket
-
-    selected_video = Enum.find(
+    {selected_video, _index} = Enum.find(
       search_result,
-      fn search -> search.video_id == selected_video["video_id"] end
-    ) |> Video.assign_user(user)
-    video_queue = video_queue
-      |> Enum.map(fn {v, _} -> v end)
+      fn {search, index} ->
+        {selected_video, _} = Integer.parse(selected_video["video_id"])
+        index == selected_video
+      end
+    )
+    selected_video = Video.assign_user(selected_video, user)
+    video_queue = Enum.map(video_queue, fn {v, _} -> v end)
       |> Queue.add_to_queue(selected_video)
 
     Phoenix.PubSub.broadcast(
