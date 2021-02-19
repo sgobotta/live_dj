9afdc174d52a952debf682d13cc81fefd6b479dd
Santiago <sgobotta@gmail.com>
2021-02-18 21:00:40 -0300

    :test_tube: ShowRoomTest: implements search video test cases

--- test/live_dj_web/live/room/show_room_test.exs
+++ test/live_dj_web/live/room/show_room_test.exs
@@ -49,7 +49,6 @@ defmodule LiveDjWeb.ShowRoomTest do
     end
   end
 
-  @tag wip: true
   describe "ShowLive chat behaviour" do
 
     @new_message_form_id "#new-message"
@@ -87,6 +86,73 @@ defmodule LiveDjWeb.ShowRoomTest do
       refute render(view) =~ "Hi?"
       assert render(view) =~ "Hello!"
       assert render(view) =~ user.username
+    end
+  end
+
+  describe "ShowLive search video behaviour" do
+
+    @search_video_form_id "#search-video-form"
+
+    setup(%{conn: conn}) do
+      %{group: group} = show_live_setup()
+
+      %{conn: conn, group: group |> Repo.preload([:permissions])}
+    end
+
+    test "As a registered User I can search and add a video to a queue",
+      %{conn: conn, group: group}
+    do
+      # Associates a group id to a new user for a new room
+      %{room: room, user: user, user_room: _user_room} = user_room_fixture(%{
+        is_owner: false, group_id: group.id
+      })
+      # Creates a new authenticated connection
+      owner_conn = log_in_user(conn, user)
+      {:ok, view, _html} = live(owner_conn, "/room/#{room.slug}")
+      # Simulates a search video interaction
+      search_query = "some video search"
+      view
+        |> element(@search_video_form_id)
+        |> render_change(%{search_field: %{query: search_query}})
+      view
+        |> element(@search_video_form_id)
+        |> render_submit(%{})
+      assert_push_event view, "receive_search_completed_signal", %{}
+      # Adds a video to the queue
+      view
+        |> element("#search-element-button-1")
+        |> render_click()
+      pos = length(room.queue) + 1
+      assert_push_event view, "video_added_to_queue", %{pos: ^pos}
+      assert_push_event view, "receive_player_state", %{}
+    end
+
+    test "As a registered User I can search and add a video to an empty queue",
+      %{conn: conn, group: group}
+    do
+      # Associates a group id to a new user for a new room
+      %{room: room, user: user, user_room: _user_room} = user_room_fixture(%{
+        is_owner: false, group_id: group.id
+      }, %{}, %{queue: []})
+      # Creates a new authenticated connection
+      owner_conn = log_in_user(conn, user)
+      {:ok, view, _html} = live(owner_conn, "/room/#{room.slug}")
+      # Simulates a search video interaction
+      search_query = "some video search"
+      view
+        |> element(@search_video_form_id)
+        |> render_change(%{search_field: %{query: search_query}})
+      view
+        |> element(@search_video_form_id)
+        |> render_submit(%{})
+      assert_push_event view, "receive_search_completed_signal", %{}
+      # Adds a video to the queue
+      view
+        |> element("#search-element-button-1")
+        |> render_click()
+      pos = length(room.queue) + 1
+      assert_push_event view, "video_added_to_queue", %{pos: ^pos}
+      assert_push_event view, "receive_player_state", %{}
     end
   end
 end
