8595c42017e4299a591536effd74442c03153042
Santiago <sgobotta@gmail.com>
2021-03-02 09:50:19 -0300

    :wrench: Recreates and exposes the the room queue

--- test/support/fixtures/organizer_fixtures.ex
+++ test/support/fixtures/organizer_fixtures.ex
@@ -6,38 +6,74 @@ defmodule LiveDj.OrganizerFixtures do
 
   alias LiveDj.AccountsFixtures
 
-  @room_queue [%{
-    next: "Wch3gJG2GJ4",
-    title: "2 Second Video",
-    img_url: "https://i.ytimg.com/vi/TK4N5W22Gts/default.jpg",
-    added_by: %{
-      uuid: "05ffb9af-ea4b-485f-9860-3264c3cdf404",
-      username: "sann"
+  @room_queue [
+    %{
+      next: "dyp2mLYhRkw",
+      title: "Video Countdown 3 seconds",
+      img_url: "https://i.ytimg.com/vi/wUF9DeWJ0Dk/default.jpg",
+      added_by: %{
+        uuid: "071db349-de96-49dd-b084-00134aeca2d1",
+        username: "guest-576460752303413630"
+      },
+      previous: "",
+      video_id: "wUF9DeWJ0Dk",
+      img_width: 120,
+      is_queued: false,
+      img_height: 90,
+      description: "my cat is epic.",
+      channel_title: "bvbb"
     },
-    previous: "UO_QuXr521I",
-    video_id: "TK4N5W22Gts",
-    img_width: 120,
-    is_queued: false,
-    img_height: 90,
-    description: "2 Second Video Sequal To 1 Second Video.",
-    channel_title: "Zetzu500"
-  },
-  %{
-    next: "tbnLqRW9Ef0",
-    title: "1 Second Video",
-    img_url: "https://i.ytimg.com/vi/Wch3gJG2GJ4/default.jpg",
-    added_by: %{
-      uuid: "05ffb9af-ea4b-485f-9860-3264c3cdf404",
-      username: "sann"
+    %{
+      next: "FJ5pRIZXVks",
+      title: "Video Countdown 20 Old  3 seconds",
+      img_url: "https://i.ytimg.com/vi/dyp2mLYhRkw/default.jpg",
+      added_by: %{
+        uuid: "9dc9c9be-5c71-4b9d-88b8-0ca362d0f28c",
+        username: "sann"
+      },
+      previous: "wUF9DeWJ0Dk",
+      video_id: "dyp2mLYhRkw",
+      img_width: 120,
+      is_queued: false,
+      img_height: 90,
+      description: "",
+      channel_title: "Bảo Anh"
     },
-    previous: "TK4N5W22Gts",
-    video_id: "Wch3gJG2GJ4",
-    img_width: 120,
-    is_queued: false,
-    img_height: 90,
-    description: "1 Second Video IT SUCKS!",
-    channel_title: "Zetzu500"
-  }]
+    %{
+      next: "qu_uJQQo_Us",
+      title: "#1 Countdown | 3 seconds with sound effect",
+      img_url: "https://i.ytimg.com/vi/FJ5pRIZXVks/default.jpg",
+      added_by: %{
+        uuid: "9dc9c9be-5c71-4b9d-88b8-0ca362d0f28c",
+        username: "sann"
+      },
+      previous: "dyp2mLYhRkw",
+      video_id: "FJ5pRIZXVks",
+      img_width: 120,
+      is_queued: false,
+      img_height: 90,
+      description: "If you read this far down the description I love you. Please Hit that ▷ SUBSCRIBE button and LIKE my video and also turn ON notifications BELL! FOLLOW ...",
+      channel_title: "DiaryBela"
+    },
+    %{
+      next: "",
+      title: "3 second video",
+      img_url: "https://i.ytimg.com/vi/qu_uJQQo_Us/default.jpg",
+      added_by: %{
+        uuid: "05ffb9af-ea4b-485f-9860-3264c3cdf404",
+        username: "wasabi"
+      },
+      previous: "FJ5pRIZXVks",
+      video_id: "qu_uJQQo_Us",
+      img_width: 120,
+      is_queued: false,
+      img_height: 90,
+      description: "I created this video with the YouTube Slideshow Creator (http://www.youtube.com/upload)",
+      channel_title: "Adam Bub"
+    }
+  ]
+
+  def room_queue, do: @room_queue
 
   def rooms_fixture do
     for _n <- 0..3 do
