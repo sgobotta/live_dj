5d939b25bdbb82dabd7c2cfb90db3fb739f70504
Santiago <sgobotta@gmail.com>
2021-03-07 17:18:45 -0300

    :computer: Slightly improves animation events handlers in the client

--- assets/js/hooks/search-syncing.js
+++ assets/js/hooks/search-syncing.js
@@ -3,21 +3,23 @@ const SearchSyncing = () => ({
     const form = document.getElementById('search-video-form')
     form.addEventListener('submit', (e) => {
       e.preventDefault()
-      this.el.classList.add('anim-fade-in')
-      this.el.children[0].classList.remove('loader-container')
-      this.el.children[0].classList.add('animate-loader-container')
+      this.el.classList.add('hidden-elements')
       setTimeout(() => {
-        this.el.classList.add('hidden-elements')
+        this.el.classList.add('anim-fade-in')
+        this.el.children[0].classList.remove('loader-container')
+        this.el.children[0].classList.add('animate-loader-container')
       }, 200)
     })
 
     this.handleEvent('receive_search_completed_signal', () => {
-      this.el.classList.remove('hidden-elements')
       this.el.classList.remove('anim-fade-in')
       this.el.classList.add('anim-fade-out')
       this.el.scrollTop = 0
       this.el.children[0].classList.add('loader-container')
       this.el.children[0].classList.remove('animate-loader-container')
+      setTimeout(() => {
+        this.el.classList.remove('hidden-elements')
+      }, 200)
     })
   }
 })
