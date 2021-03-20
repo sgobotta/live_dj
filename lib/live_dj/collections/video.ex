beb9208034bd4e961374b0e1adc5eaab3f4694c5
Santiago <sgobotta@gmail.com>
2021-03-19 23:10:10 -0300

    :computer: Creates a Video schema

---
+++ lib/live_dj/collections/video.ex
@@ -0,0 +1,23 @@
+defmodule LiveDj.Collections.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "videos" do
+    field :channel_title, :string
+    field :description, :string
+    field :img_height, :string
+    field :img_url, :string
+    field :img_width, :string
+    field :title, :string
+    field :video_id, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:channel_title, :description, :img_height, :img_url, :img_width, :title, :video_id])
+    |> validate_required([:channel_title, :description, :img_height, :img_url, :img_width, :title, :video_id])
+  end
+end
