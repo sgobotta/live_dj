937e9c27ae8b78e2a498545101cb1236cbd3dc13
Santiago <sgobotta@gmail.com>
2021-03-27 14:22:15 -0300

    :wrench: Refactors the users_videos seeds

--- priv/repo/seeds/users_videos.exs
+++ priv/repo/seeds/users_videos.exs
@@ -12,25 +12,28 @@ try do
   users = Repo.all(User) |> Enum.map(fn u -> u.id end)
   videos = Collections.list_videos() |> Enum.map(fn v -> v.id end)
 
+  # Different lists of videos for users
+  evens = Enum.filter(videos, fn e -> rem(e, 2) == 0 end)
+  odds = Enum.filter(videos, fn e -> rem(e, 2) == 0 end)
+  all = videos
+
+  videos_length = length(videos)
+  a_quarter_videos = trunc(videos_length / 4)
+  first_quarter = Enum.slice(videos, 0, a_quarter_videos * 2)
+  second_and_third_quarter = Enum.slice(videos, a_quarter_videos, a_quarter_videos * 2)
+
   # [{user_id, video_id}]
   for u <- users do
-    evens = Enum.filter(videos, fn e -> rem(e, 2) == 0 end)
-    odds = Enum.filter(videos, fn e -> rem(e, 2) == 0 end)
-    all = videos
-
-    videos_length = length(videos)
-    a_quarter_videos = trunc(videos_length / 4)
-    first_quarter = Enum.slice(videos, 0, a_quarter_videos * 2)
-    second_and_third_quarter = Enum.slice(videos, a_quarter_videos, a_quarter_videos * 2)
-
-    case u do
-      1 -> for v <- evens, do: {u, v}
-      2 -> for v <- odds, do: {u, v}
-      3 -> for v <- all, do: {u, v}
-      4 -> for v <- first_quarter, do: {u, v}
-      5 -> for v <- second_and_third_quarter, do: {u, v}
+    video_list = case u do
+      1 -> evens
+      2 -> odds
+      3 -> all
+      4 -> first_quarter
+      5 -> second_and_third_quarter
       _ -> []
     end
+
+    for v <- video_list, do: {u, v}
   end
   |> List.flatten()
   |> Enum.map(
