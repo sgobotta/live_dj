6b1731a0dbdc4075cb30183f1a47b3e85caee561
Santiago <sgobotta@gmail.com>
2021-03-30 17:43:52 -0300

    :computer: PlaylistVideo: updates schema with previous and next video fields

--- lib/live_dj/collections/playlist_video.ex
+++ lib/live_dj/collections/playlist_video.ex
@@ -3,9 +3,12 @@ defmodule LiveDj.Collections.PlaylistVideo do
   import Ecto.Changeset
 
   schema "playlists_videos" do
-    belongs_to :user, LiveDj.Accounts.User
+    belongs_to :added_by_user, LiveDj.Accounts.User
     belongs_to :playlist, LiveDj.Collections.Playlist
     belongs_to :video, LiveDj.Collections.Video
+
+    belongs_to :previous_video, LiveDj.Collections.Video
+    belongs_to :next_video, LiveDj.Collections.Video
 
     timestamps()
   end
@@ -13,7 +16,7 @@ defmodule LiveDj.Collections.PlaylistVideo do
   @doc false
   def changeset(playlist_video, attrs) do
     playlist_video
-    |> cast(attrs, [:user_id, :playlist_id, :video_id])
+    |> cast(attrs, [:added_by_user_id, :playlist_id, :video_id, :previous_video_id, :next_video_id])
     |> validate_required([:playlist_id, :video_id])
   end
 end
