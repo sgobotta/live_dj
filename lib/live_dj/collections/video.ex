24d48c692d0b1a822198855ac06edb91f4b7c128
Santiago <sgobotta@gmail.com>
2021-03-30 01:36:29 -0300

    :computer: Video schema update and validates required fields

--- lib/live_dj/collections/video.ex
+++ lib/live_dj/collections/video.ex
@@ -5,12 +5,12 @@ defmodule LiveDj.Collections.Video do
   alias LiveDj.Collections.{Playlist, PlaylistVideo, UserVideo}
 
   schema "videos" do
-    field :channel_title, :string
-    field :description, :string
+    field :channel_title, :string, default: ""
+    field :description, :string, default: ""
     field :img_height, :string
     field :img_url, :string
     field :img_width, :string
-    field :title, :string
+    field :title, :string, default: ""
     field :video_id, :string
 
     many_to_many :users, LiveDj.Accounts.User, join_through: UserVideo
@@ -23,7 +23,7 @@ defmodule LiveDj.Collections.Video do
   def changeset(video, attrs) do
     video
     |> cast(attrs, [:channel_title, :description, :img_height, :img_url, :img_width, :title, :video_id])
-    |> validate_required([:channel_title, :description, :img_height, :img_url, :img_width, :title, :video_id])
+    |> validate_required([:img_height, :img_url, :img_width, :video_id])
     |> unique_constraint(:video_id)
   end
 
