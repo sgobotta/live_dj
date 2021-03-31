60cbe15b221e146d9a607d04393ffebff3d07566
Santiago <sgobotta@gmail.com>
2021-03-31 00:40:37 -0300

    :computer: QueueControls: creates or updates playlists_videos relationships on queue save

--- lib/live_dj_web/components/queue_controls/queue_controls.ex
+++ lib/live_dj_web/components/queue_controls/queue_controls.ex
@@ -27,6 +27,14 @@ defmodule LiveDjWeb.Components.QueueControls do
     video_queue = Enum.map(video_queue, fn {v, _} -> v end)
     {:ok, _room} = Organizer.update_room(room, %{queue: video_queue})
 
+    # Recreates a playlists_videos relationships
+    playlists_videos = Enum.map(Enum.with_index(video_queue), fn {video, index} ->
+      LiveDj.Collections.cast_playlist_video(
+        Map.merge(video, %{position: index}), room.playlist_id
+      )
+    end)
+    LiveDj.Collections.create_or_update_playlists_videos(playlists_videos)
+
     Phoenix.PubSub.broadcast(
       LiveDj.PubSub,
       "room:" <> slug,
