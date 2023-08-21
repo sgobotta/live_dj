ffe676a39cead19990c53741d7443cf2272a5d86
Santiago Botta <sgobotta@gmail.com>
2023-08-20 23:48:32 -0300

    Update layout and styles

--- lib/livedj_web/router.ex
+++ lib/livedj_web/router.ex
@@ -23,8 +23,9 @@ defmodule LivedjWeb.Router do
     get "/", PageController, :home
 
     live_session :sessions,
-      root_layout: {LivedjWeb.Layouts, :root_session},
-      layout: {LivedjWeb.Layouts, :session} do
+      layout: {LivedjWeb.Layouts, :session},
+      on_mount: [{LivedjWeb.UserAuth, :mount_current_user}],
+      root_layout: {LivedjWeb.Layouts, :root_session} do
       scope "/sessions", Sessions do
         live "/rooms/:id", RoomLive.Show, :show
       end
