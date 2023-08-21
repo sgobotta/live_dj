6e526dfa354dd36da9f4c2e0e80b11167f631777
Santiago Botta <sgobotta@gmail.com>
2023-08-20 23:47:58 -0300

    Add a form to add media

--- lib/livedj_web/components/core_components.ex
+++ lib/livedj_web/components/core_components.ex
@@ -247,6 +247,55 @@ defmodule LivedjWeb.CoreComponents do
   end
 
   @doc """
+  Renders a simple form for search use cases.
+
+  ## Examples
+
+      <.add_media_form for={@form} phx-change="validate" phx-submit="save">
+        <.input field={@form[:email]} label="Email"/>
+        <.input field={@form[:username]} label="Username" />
+        <:actions>
+          <.button>Save</.button>
+        </:actions>
+      </.add_media_form>
+  """
+  attr :for, :any, required: true, doc: "the datastructure for the form"
+
+  attr :as, :any,
+    default: nil,
+    doc: "the server side parameter to collect all input under"
+
+  attr :rest, :global,
+    include:
+      ~w(autocomplete name rel action enctype method novalidate target multipart),
+    doc: "the arbitrary HTML attributes to apply to the form tag"
+
+  slot :inner_block, required: true
+  slot :field, doc: "the slot for the form input, such as a text input"
+  slot :actions, doc: "the slot for form actions, such as a submit button"
+
+  def add_media_form(assigns) do
+    ~H"""
+    <.form :let={f} for={@for} as={@as} {@rest}>
+      <div class="mt-8 space-y-8 bg-white w-full px-2">
+        <div class="flex flex-row">
+          <div class="w-full">
+            <%= render_slot(@field, f) %>
+          </div>
+          <div
+            :for={action <- @actions}
+            class="mt-2 flex items-center justify-between gap-6"
+          >
+            <%= render_slot(action, f) %>
+          </div>
+        </div>
+        <%= render_slot(@inner_block, f) %>
+      </div>
+    </.form>
+    """
+  end
+
+  @doc """
   Renders a button.
 
   ## Examples
