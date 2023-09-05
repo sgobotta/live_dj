47535ce15e8bb199970d70e819e4927b60c1afdd
Santiago Botta <sgobotta@gmail.com>
2023-09-05 03:02:30 -0300

    Add Video type and struct transformation functions

--- lib/livedj/media/video.ex
+++ lib/livedj/media/video.ex
@@ -4,6 +4,8 @@ defmodule Livedj.Media.Video do
   import Ecto.Changeset
   import LivedjWeb.Gettext
 
+  @type t :: %__MODULE__{}
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "videos" do
@@ -36,5 +38,50 @@ defmodule Livedj.Media.Video do
     |> unique_constraint(:external_id,
       message: dgettext("errors", "video with this id already exists")
     )
+  end
+
+  @spec from_struct(t()) :: map()
+  def from_struct(%__MODULE__{
+        id: id,
+        etag: etag,
+        external_id: external_id,
+        published_at: published_at,
+        thumbnail_url: thumbnail_url,
+        title: title,
+        inserted_at: inserted_at,
+        updated_at: updated_at
+      }),
+      do: %{
+        id: id,
+        etag: etag,
+        external_id: external_id,
+        published_at: published_at,
+        thumbnail_url: thumbnail_url,
+        title: title,
+        inserted_at: inserted_at,
+        updated_at: updated_at
+      }
+
+  @spec from_hset(map()) :: t()
+  def from_hset(%{
+        id: id,
+        etag: etag,
+        external_id: external_id,
+        published_at: published_at,
+        thumbnail_url: thumbnail_url,
+        title: title,
+        inserted_at: inserted_at,
+        updated_at: updated_at
+      }) do
+    %__MODULE__{
+      id: id,
+      etag: etag,
+      external_id: external_id,
+      published_at: published_at,
+      thumbnail_url: thumbnail_url,
+      title: title,
+      inserted_at: inserted_at,
+      updated_at: updated_at
+    }
   end
 end
