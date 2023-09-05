23dcb552928f43a08bb74b6d817ec08daeca7cf8
Santiago Botta <sgobotta@gmail.com>
2023-09-05 03:46:43 -0300

    Create a CachedMedia module

---
+++ lib/livedj/media/cached_media.ex
@@ -0,0 +1,13 @@
+defmodule Livedj.Media.CachedMedia do
+  @moduledoc false
+
+  @key_prefix "media"
+
+  @spec insert(String.t(), map()) :: {:ok, map()} | {:error, :hset_error}
+  def insert(key, value) do
+    Redis.Hash.hset(build_key(key), value)
+  end
+
+  @spec build_key(Ecto.UUID.t()) :: String.t()
+  defp build_key(key), do: @key_prefix <> ":" <> key
+end
