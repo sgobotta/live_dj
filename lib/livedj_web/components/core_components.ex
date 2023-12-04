416ea06921c5f0cf20ad88cf231f8b8111e6228d
Santiago Botta <sgobotta@gmail.com>
2023-12-04 03:09:04 -0300

    Update the input component to support a class prop

--- lib/livedj_web/components/core_components.ex
+++ lib/livedj_web/components/core_components.ex
@@ -364,6 +364,7 @@ defmodule LivedjWeb.CoreComponents do
   attr :name, :any
   attr :label, :string, default: nil
   attr :value, :any
+  attr :class, :string, default: ""
 
   attr :type, :string,
     default: "text",
@@ -480,12 +481,14 @@ defmodule LivedjWeb.CoreComponents do
         name={@name}
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
-        class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
-          @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
-        ]}
+        class={
+          [
+            "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+            "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+            @errors == [] && "border-zinc-300 focus:border-zinc-400",
+            @errors != [] && "border-rose-400 focus:border-rose-400"
+          ] ++ [@class]
+        }
         {@rest}
       />
       <.error :for={msg <- @errors}><%= msg %></.error>
