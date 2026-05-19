30f0653e24100de5c4b6d32ba88700ea8c297f64
Santiago Botta <sgobotta@gmail.com>
2026-05-19 00:35:06 -0300

    Consolidate user settings into a single form
      Replace three separate forms (username, email, password) with one
      combined form and submit button. The email field uses a separate
      user[new_email] param to avoid colliding with the hidden user[email]
      value needed for the password re-login POST action.

--- lib/livedj_web/live/user_settings_live.ex
+++ lib/livedj_web/live/user_settings_live.ex
@@ -10,89 +10,45 @@ defmodule LivedjWeb.UserSettingsLive do
       <:subtitle>Manage your account email address and password settings</:subtitle>
     </.header>
 
-    <div class="space-y-12 divide-y">
-      <div>
-        <.simple_form
-          for={@username_form}
-          id="username_form"
-          phx-submit="update_username"
-          phx-change="validate_username"
-        >
-          <.input
-            field={@username_form[:username]}
-            type="text"
-            label="Username"
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Saving...">Save Username</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-      <div>
-        <.simple_form
-          for={@email_form}
-          id="email_form"
-          phx-submit="update_email"
-          phx-change="validate_email"
-        >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
-          <.input
-            field={@email_form[:current_password]}
-            name="current_password"
-            id="current_password_for_email"
-            type="password"
-            label="Current password"
-            value={@email_form_current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-      <div>
-        <.simple_form
-          for={@password_form}
-          id="password_form"
-          action={~p"/users/log_in?_action=password_updated"}
-          method="post"
-          phx-change="validate_password"
-          phx-submit="update_password"
-          phx-trigger-action={@trigger_submit}
-        >
-          <.input
-            field={@password_form[:email]}
-            type="hidden"
-            id="hidden_user_email"
-            value={@current_email}
-          />
-          <.input
-            field={@password_form[:password]}
-            type="password"
-            label="New password"
-            required
-          />
-          <.input
-            field={@password_form[:password_confirmation]}
-            type="password"
-            label="Confirm new password"
-          />
-          <.input
-            field={@password_form[:current_password]}
-            name="current_password"
-            type="password"
-            label="Current password"
-            id="current_password_for_password"
-            value={@current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Password</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-    </div>
+    <.simple_form
+      for={@settings_form}
+      id="settings_form"
+      action={~p"/users/log_in?_action=password_updated"}
+      method="post"
+      phx-submit="update_settings"
+      phx-trigger-action={@trigger_submit}
+    >
+      <%!-- Supplies user[email] for the re-login POST after a password change --%>
+      <input type="hidden" name="user[email]" value={@current_email} />
+      <.input field={@settings_form[:username]} type="text" label="Username" />
+      <.input
+        field={@settings_form[:new_email]}
+        type="email"
+        label="Email"
+        required
+      />
+      <.input
+        field={@settings_form[:password]}
+        type="password"
+        label="New password"
+      />
+      <.input
+        field={@settings_form[:password_confirmation]}
+        type="password"
+        label="Confirm new password"
+      />
+      <.input
+        field={@settings_form[:current_password]}
+        name="current_password"
+        id="current_password"
+        type="password"
+        label="Current password"
+        value={@current_password}
+      />
+      <:actions>
+        <.button phx-disable-with="Saving...">Save</.button>
+      </:actions>
+    </.simple_form>
     """
   end
 
@@ -115,129 +71,119 @@ defmodule LivedjWeb.UserSettingsLive do
 
   def mount(_params, _session, socket) do
     user = socket.assigns.current_user
-    email_changeset = Accounts.change_user_email(user)
-    password_changeset = Accounts.change_user_password(user)
-    username_changeset = Accounts.change_user_username(user)
 
     socket =
       socket
       |> assign(:current_password, nil)
-      |> assign(:email_form_current_password, nil)
       |> assign(:current_email, user.email)
-      |> assign(:email_form, to_form(email_changeset))
-      |> assign(:password_form, to_form(password_changeset))
-      |> assign(:username_form, to_form(username_changeset))
       |> assign(:trigger_submit, false)
+      |> assign(:settings_form, settings_form(user))
 
     {:ok, socket}
   end
 
-  def handle_event("validate_username", %{"user" => user_params}, socket) do
-    username_form =
-      socket.assigns.current_user
-      |> Accounts.change_user_username(user_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply, assign(socket, :username_form, username_form)}
-  end
-
-  def handle_event("update_username", %{"user" => user_params}, socket) do
+  def handle_event(
+        "update_settings",
+        %{"current_password" => current_password, "user" => user_params},
+        socket
+      ) do
     user = socket.assigns.current_user
 
-    case Accounts.update_user_username(user, user_params) do
+    {socket, trigger_submit} =
+      {socket, false}
+      |> apply_username_change(user, user_params)
+      |> apply_email_change(user, user_params, current_password)
+      |> apply_password_change(user, user_params, current_password)
+
+    {:noreply,
+     socket
+     |> assign(:current_password, current_password)
+     |> assign(:trigger_submit, trigger_submit)}
+  end
+
+  defp apply_username_change({socket, trigger}, user, %{"username" => username})
+       when username != "" do
+    case Accounts.update_user_username(user, %{"username" => username}) do
       {:ok, updated_user} ->
-        username_form =
-          updated_user
-          |> Accounts.change_user_username()
-          |> to_form()
-
-        {:noreply,
-         socket
-         |> put_flash(:info, "Username updated successfully.")
+        {socket
          |> assign(:current_user, updated_user)
-         |> assign(:username_form, username_form)}
+         |> assign(:settings_form, settings_form(updated_user))
+         |> put_flash(:info, "Username updated."), trigger}
 
-      {:error, changeset} ->
-        {:noreply, assign(socket, :username_form, to_form(changeset))}
+      {:error, _} ->
+        {put_flash(socket, :error, "Could not update username."), trigger}
     end
   end
 
-  def handle_event("validate_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
+  defp apply_username_change(acc, _user, _params), do: acc
 
-    email_form =
-      socket.assigns.current_user
-      |> Accounts.change_user_email(user_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
+  defp apply_email_change(
+         {socket, trigger},
+         user,
+         %{"new_email" => new_email},
+         current_password
+       )
+       when new_email != "" do
+    if new_email != user.email do
+      case Accounts.apply_user_email(user, current_password, %{
+             "email" => new_email
+           }) do
+        {:ok, applied_user} ->
+          Accounts.deliver_user_update_email_instructions(
+            applied_user,
+            user.email,
+            &url(~p"/users/settings/confirm_email/#{&1}")
+          )
 
-    {:noreply,
-     assign(socket,
-       email_form: email_form,
-       email_form_current_password: password
-     )}
+          {put_flash(socket, :info, "Confirmation sent to #{new_email}."),
+           trigger}
+
+        {:error, _} ->
+          {put_flash(
+             socket,
+             :error,
+             "Could not update email. Check your current password."
+           ), trigger}
+      end
+    else
+      {socket, trigger}
+    end
   end
 
-  def handle_event("update_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-    user = socket.assigns.current_user
+  defp apply_email_change(acc, _user, _params, _password), do: acc
 
-    case Accounts.apply_user_email(user, password, user_params) do
-      {:ok, applied_user} ->
-        Accounts.deliver_user_update_email_instructions(
-          applied_user,
-          user.email,
-          &url(~p"/users/settings/confirm_email/#{&1}")
-        )
+  defp apply_password_change(
+         {socket, _trigger},
+         user,
+         %{"password" => password} = user_params,
+         current_password
+       )
+       when password != "" do
+    case Accounts.update_user_password(user, current_password, user_params) do
+      {:ok, _user} ->
+        {socket, true}
 
-        info =
-          "A link to confirm your email change has been sent to the new address."
-
-        {:noreply,
-         socket
-         |> put_flash(:info, info)
-         |> assign(email_form_current_password: nil)}
-
-      {:error, changeset} ->
-        {:noreply,
-         assign(
+      {:error, _} ->
+        {put_flash(
            socket,
-           :email_form,
-           to_form(Map.put(changeset, :action, :insert))
-         )}
+           :error,
+           "Could not update password. Check your current password."
+         ), false}
     end
   end
 
-  def handle_event("validate_password", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
+  defp apply_password_change(acc, _user, _params, _password), do: acc
 
-    password_form =
-      socket.assigns.current_user
-      |> Accounts.change_user_password(user_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply,
-     assign(socket, password_form: password_form, current_password: password)}
-  end
-
-  def handle_event("update_password", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-    user = socket.assigns.current_user
-
-    case Accounts.update_user_password(user, password, user_params) do
-      {:ok, user} ->
-        password_form =
-          user
-          |> Accounts.change_user_password(user_params)
-          |> to_form()
-
-        {:noreply,
-         assign(socket, trigger_submit: true, password_form: password_form)}
-
-      {:error, changeset} ->
-        {:noreply, assign(socket, password_form: to_form(changeset))}
-    end
+  defp settings_form(user) do
+    to_form(
+      %{
+        "username" => user.username,
+        "new_email" => user.email,
+        "password" => "",
+        "password_confirmation" => "",
+        "current_password" => ""
+      },
+      as: :user
+    )
   end
 end
