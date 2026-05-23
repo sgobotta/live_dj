6612106763fa9a8fc7056a3ee9f7f702fab89cd8
Santiago Botta <sgobotta@gmail.com>
2026-05-23 19:43:05 -0300

    Playlist: background colors update

--- lib/livedj_web/components/list_component.ex
+++ lib/livedj_web/components/list_component.ex
@@ -16,8 +16,8 @@ defmodule LivedjWeb.ListComponent do
             class={"
               first:mt-0 last:mb-0
               #{if current_media?(@current_media, item.external_id),
-                do: "text-green-500 dark:text-green-500 bg-zinc-200 dark:bg-zinc-900 hover:bg-zinc-200 dark:hover:bg-zinc-500",
-                else: "text-zinc-900 dark:text-zinc-100 bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-900 dark:hover:bg-zinc-800"
+                do: "text-green-500 dark:text-green-500 bg-zinc-200 dark:bg-zinc-700",
+                else: "text-zinc-900 dark:text-zinc-100 bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-900 dark:hover:bg-zinc-700"
               }
               #{if @state == :locked, do: "border-dashed", else: ""}
               my-1 rounded-lg border-zinc-300 dark:border-zinc-700 border-[0px]
