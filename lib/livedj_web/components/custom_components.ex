64ae6d79233e1911d4b8d838e95b9a17bb3322a5
Santiago Botta <sgobotta@gmail.com>
2026-08-15 15:27:43 -0300

    Update tooltip horizontal padding to px-1

--- lib/livedj_web/components/custom_components.ex
+++ lib/livedj_web/components/custom_components.ex
@@ -24,7 +24,7 @@ defmodule LivedjWeb.CustomComponents do
     ~H"""
     <span class={[
       "absolute hidden group-hover:block",
-      "whitespace-nowrap rounded-md px-2 py-1",
+      "whitespace-nowrap rounded-md px-1 py-1",
       "bg-tone-800 dark:bg-tone-200",
       "text-xs text-tone-100 dark:text-tone-900",
       "shadow-md pointer-events-none",
